532bfffb33b542367a14fb1188571de9c104254c
Kim <richards.kimc@gmail.com>
2025-09-30 08:17:35 -0400

    first commit

---
+++ src/com/simple/account/BankAccount.java
@@ -0,0 +1,48 @@
+package com.simple.account;
+
+public class BankAccount {
+    private int accountID;
+    private String ownerName;
+    private float balance;
+
+    public BankAccount(int accountID, String ownerName, float balance){
+        super(); // Call to the superclass's constructor
+        this.accountID = accountID;
+        this.ownerName = ownerName;
+        setBalance(balance); // Use method because there is validation logic
+    }
+
+    public void deposit(float amount){
+        balance = balance + amount;
+    }
+
+    public void setAccountID(int newID){
+        this.accountID = newID;
+    }
+
+    public int getAccountID(){
+        return this.accountID;
+    }
+
+    public void setOwnerName(String newName){
+        this.ownerName = newName;
+    }
+
+    public String getOwnerName(){
+        return this.ownerName;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        // If the balance is bigger than 0, set it
+        if(balance > 0f){
+            this.balance = balance;
+        // If the balance is not bigger than 0, print error
+        }else{
+            System.out.println("Error. Balance cannot be a negative.");
+        }
+    }
+}
